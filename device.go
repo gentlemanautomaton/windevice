d74d284627b1657c7feac22154a563e4bc6f55a0
Joshua Sjoding <ga@josh.hey.nu>
2019-02-27 01:26:25 -0800

    Reordered device methods to match property order

--- device.go
+++ device.go
@@ -19,6 +19,11 @@ func (device Device) Sys() (list syscall.Handle, entry setupapi.DevInfoData) {
 	return device.list, device.entry
 }
 
+// Description returns the description of the device.
+func (device Device) Description() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Description)
+}
+
 // HardwareID returns the set of hardware IDs associated with the device.
 func (device Device) HardwareID() ([]string, error) {
 	return setupapi.GetDeviceRegistryStrings(device.list, device.entry, deviceproperty.HardwareID)
@@ -29,14 +34,9 @@ func (device Device) CompatibleID() ([]string, error) {
 	return setupapi.GetDeviceRegistryStrings(device.list, device.entry, deviceproperty.CompatibleID)
 }
 
-// Description returns the description of the device.
-func (device Device) Description() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Description)
-}
-
-// FriendlyName returns the friendly name of the device.
-func (device Device) FriendlyName() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.FriendlyName)
+// Service returns the service for the device.
+func (device Device) Service() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Service)
 }
 
 // Class returns the class name of the device.
@@ -44,14 +44,9 @@ func (device Device) Class() (string, error) {
 	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Class)
 }
 
-// EnumeratorName returns the name of the device's enumerator.
-func (device Device) EnumeratorName() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.EnumeratorName)
-}
-
-// LocationInformation returns the location information for the device.
-func (device Device) LocationInformation() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.LocationInformation)
+// Driver returns the driver for the device.
+func (device Device) Driver() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Driver)
 }
 
 // Manufacturer returns the manufacturer of the device.
@@ -59,17 +54,22 @@ func (device Device) Manufacturer() (string, error) {
 	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Manufacturer)
 }
 
+// FriendlyName returns the friendly name of the device.
+func (device Device) FriendlyName() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.FriendlyName)
+}
+
+// LocationInformation returns the location information for the device.
+func (device Device) LocationInformation() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.LocationInformation)
+}
+
 // PhysicalDeviceObjectName returns the physical object name of the device.
 func (device Device) PhysicalDeviceObjectName() (string, error) {
 	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.PhysicalDeviceObjectName)
 }
 
-// Driver returns the driver for the device.
-func (device Device) Driver() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Driver)
-}
-
-// Service returns the service for the device.
-func (device Device) Service() (string, error) {
-	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.Service)
+// EnumeratorName returns the name of the device's enumerator.
+func (device Device) EnumeratorName() (string, error) {
+	return setupapi.GetDeviceRegistryString(device.list, device.entry, deviceproperty.EnumeratorName)
 }
